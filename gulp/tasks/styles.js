ad125f8dc5335c0c5eec9c27d0f1ea5e517a13d6
Max <maxdaviscompany@gmail.com>
2018-08-19 16:52:53 -0400

    styled header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -10,7 +10,7 @@ hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
   return gulp.src('./assets/styles/styles.css')
-  .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
   .on('error', function(errorInfo) {
     console.log(errorInfo.toString());
     this.emit('end');
